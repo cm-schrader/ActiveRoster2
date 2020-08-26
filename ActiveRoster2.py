3347ed2226be80badf98759978db875b09f49027
Christian M. Schrader <cmschrader@wpi.edu>
2020-08-26 12:03:13 -0400

    Initial commit

---
+++ ActiveRoster2.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+import os
+
+members = defaultdict(lambda: defaultdict(lambda: 0))
+
+
+def read_csv(path):
+    pass
+
+
+def read_directory(path):
+    pass
+
+
+def run_analysis():
+    pass
+
+
+if __name__ == "__main__":
+    pass
