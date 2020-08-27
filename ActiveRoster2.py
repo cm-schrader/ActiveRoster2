8b0cc06f7c7c14889d91c8c6705ffb532b189536
Christian M. Schrader <cmschrader@wpi.edu>
2020-08-26 20:23:12 -0400

    All essential features work

--- ActiveRoster2.py
+++ ActiveRoster2.py
@@ -4,16 +4,17 @@ import os
 
 events = defaultdict(lambda: 0)
 members = defaultdict(lambda: defaultdict(lambda: 0))
+active_members = 0
 
 
 def read_csv(directory, file):
     counted = list()
     events[directory] += 1
-    with open(f"{directory}/{file}", "r") as csv:
+    with open(f"{directory}/{file}", "r", encoding="utf-16-le") as csv:
         line = csv.readline()
         for line in csv:
             cells = str(line).split("\t")[0:2]
-            if cells[0] != '\x00' and cells[0] not in counted and cells[1] == '\x00J\x00o\x00i\x00n\x00e\x00d\x00':
+            if cells[0] != '\x00' and cells[0] not in counted and cells[1] == "Joined":
                 members[cells[0]][directory] += 1
 
 
@@ -22,7 +23,9 @@ def read_directory(path):
         read_csv(path, file)
 
 
-def run_analysis():
+def analyze_csvs():
+    events.clear()
+    members.clear()
     for directory in next(os.walk('.'))[1]:
         if directory[0] == ".":
             continue
@@ -30,11 +33,21 @@ def run_analysis():
 
 
 def member_str(name):
-    string = f"{white_to_len(name, 60)}|"
-    for key in members[name].keys():
-        ratio = 100 / events[key] * members[name][key]
-        string = string + f"\t{key}: {white_to_len(str(round(ratio, 1)), 6)}%"
+    string = f"{white_to_len(name, 30)}"
+    for key in events.keys():
+        string = string + \
+            f"    {key}: {white_to_len(str(get_ratio(name, key)), 6)}%"
     return string
+
+
+def get_ratio(name, group):
+    return round(100 / events[group] * members[name][group], 1)
+
+
+def is_active(name, group):
+    if get_ratio(name, group) >= 75:
+        return True
+    return False
 
 
 def white_to_len(string, length):
@@ -51,24 +64,103 @@ def white_to_len(string, length):
     return string
 
 
+def rgb_to_tk(rgb):
+    """
+    Converts rgb values to tkinter color codes.
+    :param rgb: Tuple of 3 ints.
+    :return: tk color code string
+    """
+    return "#%02x%02x%02x" % rgb
+
+
 class Window(tk.Tk):
     def __init__(self):
+        # Window initialization
         super().__init__()
         self.title("ActiveRoster2 : HPRC")
-        tk.Button(self, text="Recalculate", width=15,
-                  command=run_analysis).pack()
-        self.listbox = tk.Listbox(self, width=200, font="TkFixedFont")
-        self.listbox.pack()
-        for member in members.keys():
-            self.listbox.insert(tk.END, member_str(member))
+        self.frame = tk.Frame(self)
+        self.frame.pack()
         self.protocol("WM_DELETE_WINDOW", self.close_window)
+
+        # Event Info
+        self.event_labels = dict()
+        self.event_frame = tk.Frame(self.frame)
+        self.event_frame.grid(row=0, column=2, sticky=tk.N)
+        tk.Label(self.event_frame, text="Events", font='Helvetica 12 bold').grid(
+            row=0, column=0, columnspan=2, sticky=tk.N)
+        eventRow = 1
+        for event_type in events.keys():
+            tk.Label(self.event_frame, text=f"{event_type}:", anchor=tk.E).grid(
+                row=eventRow, column=0)
+            self.event_labels[event_type] = tk.Label(
+                self.event_frame, text=str(events[event_type]), anchor=tk.W)
+            self.event_labels[event_type].grid(
+                row=eventRow, column=1, sticky=tk.N)
+            eventRow += 1
+
+        # Member Info
+        self.member_labels = dict()
+        self.member_frame = tk.Frame(self.frame)
+        self.member_frame.grid(row=0, column=1, sticky=tk.N)
+        tk.Label(self.member_frame, text="Members", font='Helvetica 12 bold').grid(
+            row=0, column=0, columnspan=2, sticky=tk.N)
+        tk.Label(self.member_frame, text="Total Active Members:",
+                 anchor=tk.E).grid(row=1, column=0, sticky=tk.N)
+        self.total_active = tk.Label(
+            self.member_frame, text=str(active_members), anchor=tk.W)
+        self.total_active.grid(row=1, column=1, sticky=tk.N)
+        for event_type in events.keys():
+            tk.Label(self.member_frame, text=f"Active in {event_type}:", anchor=tk.E).grid(
+                row=eventRow, column=0)
+            self.member_labels[event_type] = tk.Label(
+                self.member_frame, text="##", anchor=tk.W)
+            self.member_labels[event_type].grid(
+                row=eventRow, column=1, sticky=tk.N)
+            eventRow += 1
+
+        # Main components
+        tk.Button(self.frame, text="Recalculate", width=15,
+                  command=self.recalculate, bg=rgb_to_tk((252, 197, 68))).grid(row=0, column=0, sticky=tk.W + tk.S + tk.E)
+
+        self.listbox = tk.Listbox(
+            self.frame, width=120, height=30, font="TkFixedFont")
+        self.listbox.grid(row=1, column=0, sticky=tk.W + tk.E, columnspan=3)
+        listscroll = tk.Scrollbar(self.frame)
+        listscroll.grid(row=1, column=4, sticky=tk.N + tk.S)
+        self.listbox.config(yscrollcommand=listscroll.set)
+        listscroll.config(command=self.listbox.yview)
+        self.recalculate()
+
+    def recalculate(self):
+        analyze_csvs()
+        self.listbox.delete(0, tk.END)
+        active_members = 0
+        group_members = defaultdict(lambda: 0)
+        index = 0
+        for member in members.keys():
+            active_groups = 0
+            self.listbox.insert(tk.END, member_str(member))
+            for event_type in events.keys():
+                activity = is_active(member, event_type)
+                if activity:
+                    group_members[event_type] += 1
+                    active_groups += 1
+            if active_groups >= 2:
+                self.listbox.itemconfig(index, bg=rgb_to_tk((102, 255, 102)))
+                active_members += 1
+            else:
+                self.listbox.itemconfig(index, bg=rgb_to_tk((255, 102, 102)))
+            index += 1
+        self.total_active.config(text=str(active_members))
+
+        for label in self.event_labels.keys():
+            self.event_labels[label].config(text=str(events[label]))
+            self.member_labels[label].config(text=str(group_members[label]))
 
     def close_window(self):
         self.destroy()
 
 
 if __name__ == "__main__":
-    run_analysis()
+    analyze_csvs()
     Window().mainloop()
-    for member in members.keys():
-        print(member_str(member))
